c7c8fdce6300f0a35b6d0db7052f1759a3c48a48
ANASS OULED BEN TAHAR <Anass.taher@gmail.com>
2021-12-17 17:39:24 +0100

    first commit

---
+++ back/routes/api.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\RoleController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+// Route::resource('roles', ProductController::class);
+
+// Public routes
+Route::get('/roles', [RoleController::class, 'index']);
+Route::get('/roles/{id}', [RoleController::class, 'show']);
+Route::post('/roles', [RoleController::class, 'store']);
+Route::put('/roles/{id}', [RoleController::class, 'update']);
+Route::delete('/roles/{id}', [RoleController::class, 'destroy']);
